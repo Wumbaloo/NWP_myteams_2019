c4b2d1cf1350307e1f1f343fc7f9007bd2eb35d9
William <william.gaudfrin@epitech.eu>
2020-05-03 16:08:08 +0200

    [~] Add some comments of Anicotte to know what left to do

--- server/src/commands/log_in_out.c
+++ server/src/commands/log_in_out.c
@@ -17,10 +17,13 @@ void login_cmd(myteams_t *teams, client_t *client, void *arg)
     char uuid[36];
     client_t *temp = get_client_by_username(teams->client_head, arg);
 
+    //User déjà connecté
     if (temp && temp->is_connected == true)
         duplicate_client(temp, client);
+    //User reprend sa session
     else if (temp && temp->is_connected == false)
         temp->is_connected = true;
+    //User : première arrivée
     else {
         uuid_generate(client->user_uuid);
         memcpy(client->user_name, arg, DEFAULT_NAME_LENGTH);
@@ -28,17 +31,17 @@ void login_cmd(myteams_t *teams, client_t *client, void *arg)
     }
     uuid_unparse(client->user_uuid, uuid);
     server_event_user_logged_in(uuid);
-    char *test = strdup(strcat("230 ", uuid));
-    printf("%s\n", test);
-    client->reply = test;
+    client->reply = strdup("230 UUID");
+    //Envoyer réponse au client
 }
-//TODO: Envoyer la réponse au client
+
 void logout_cmd(myteams_t *teams, client_t *client, void *arg)
 {
     char uuid[36];
 
     (void)(arg);
     if (client->is_connected == false) {
+        //Error not logged in
         return;
     }
     client->is_connected = false;
@@ -48,4 +51,5 @@ void logout_cmd(myteams_t *teams, client_t *client, void *arg)
     teams->clients[teams->act_idx] = 0;
     uuid_unparse(client->user_uuid, uuid);
     server_event_user_logged_out(uuid);
+    //Envoyer réponse au client
 }
